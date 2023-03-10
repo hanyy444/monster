31f4b0375f492055902604656b04989683ae07e2
hanyy444 <ahmed.haanyyy@gmail.com>
2023-03-10 22:50:05 +0200

    removed routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
-import { lazy, Suspense } from 'react';
 import HomePage from 'pages/home/home.page'
 import GlobalStyle from 'assets/styles/global-styles';
-
-// Pages
-// const SignInPage = lazy(() => import('pages/sign-in/sign-in.page'));
-
 
 function App() {
   return (
     <div className='app'>
       <GlobalStyle />
-      <Suspense>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          {/* <Route exact path='/sign-in' element={<SignInPage />} />
-          <Route exact path='/sign-up' element={<SignInPage />} /> */}
-        </Routes>
-      </Suspense>
+      <HomePage />
     </div>
   );
 }
